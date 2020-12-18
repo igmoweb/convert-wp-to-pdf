31bfd902640c2b223b468e54ccd5c16cec8536de
Ignacio Cruz <igmoweb@gmail.com>
2020-12-18 18:02:37 +0100

    Second commit :)

---
+++ README.md
@@ -0,0 +1,7 @@
+# Convert WP REST API to PDF
+
+`docker-compose up`
+
+`docker-compose exec server composer install`
+
+Visit `localhost:8080`
--- index.php
+++ index.php
@@ -1,15 +1,72 @@
 <?php
-
 require_once __DIR__ . '/vendor/autoload.php';
 
-$curl = new Curl\Curl();
-$curl->setHeader('Content-Type', 'application/json');
-$curl->get('https://public-api.wordpress.com/wp/v2/sites/igmoweb.com/posts');
-$posts = json_decode($curl->response);
-$mpdf = new \Mpdf\Mpdf();
-foreach ( $posts as $post ) {
-	$mpdf->WriteHTML("<h1>{$post->title->rendered}</h1>");
-	$mpdf->WriteHTML($post->content->rendered);
+function process() {
+	$url = $_POST['url'] ?? '';
+	if ( ! $url ) {
+		die( 'Empty URL' );
+	}
+
+	$curl = new Curl\Curl();
+	$curl->setHeader('Content-Type', 'application/json');
+	$curl->get($url);
+	$posts = json_decode($curl->response);
+
+	if ($curl->error) {
+		echo $curl->error_message;
+		die();
+	}
+
+
+	$mpdf = new \Mpdf\Mpdf(['tempDir' => '/tmp']);
+	foreach ( $posts as $post ) {
+		$mpdf->WriteHTML("<h1>{$post->title->rendered}</h1>");
+		$mpdf->WriteHTML($post->content->rendered);
+		$mpdf->AddPage();
+	}
+
+	$mpdf->Output();
+	die();
 }
 
-$mpdf->Output();
+$action = $_POST['action'] ?? '';
+if ( $action === 'getpdf' ) {
+	process();
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>To PDF</title>
+	<style>
+		body {
+			line-height: 1.5;
+		}
+		form {
+			padding: 2rem;
+			width: 80%;
+			margin: 0 auto;
+			background: cornflowerblue;
+		}
+		form input[type="text"] {
+			width: 100%;
+		}
+	</style>
+</head>
+<body>
+<form action="" method="post">
+	<label>
+		REST API URL to posts (ex. site.com/wp-json/wp/v2/posts)
+		<input type="text" name="url">
+	</label>
+	<p>By default WP will return 10 posts but you can use the per_page=20 parameter in the URL to bring more but don't be too greedy.</p>
+	<input type="submit" value="Submit">
+	<input type="hidden" name="action" value="getpdf">
+</form>
+</body>
+</html>
+
